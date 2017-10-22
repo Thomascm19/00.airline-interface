f28f5a4bc19316db03ef6a5f865e8c6e424f23a6
thomas.caycedo <thomascaycedo19@outlook.com>
2017-10-21 23:49:53 -0500

    En las clases se agreagan sus atributos

--- Proyecto Final/src/proyecto/pkgfinal/AerolineaMain.java
+++ Proyecto Final/src/proyecto/pkgfinal/AerolineaMain.java
@@ -1,21 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package proyecto.pkgfinal;
 
-/**
- *
- * @author Thomas-CM
- */
+
 public class AerolineaMain {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+    Aerolinea AM = new Aerolinea ();
+    AM.setNombre("AM");
+    AM.setNit(1-2131231);
+    AM.setDireccion("KR 12 #52-84 ");
+    AM.setTelefono(8734672);
+    AM.setEmpleados(475);
+    AM.setAviones(47);
+    
+        Aviones AvionesAerolinea = new Aviones(47, "ACPM", "Charter" , "Rojo", 23, 1321,1231,12312);
+        RutaOperacion RutaOperacion = new RutaOperacion(23,23,23,23,23,23);
+        Personal personal = new Personal(213, 123, 123, 123, 123);
+
+      
+        
+        System.out.println("La aerolinea tiene las siguientes caracteristicas: ");
+        System.out.println("" +AM.getNombre()+ "\n"
+                + AM.getNit() + "\n"
+                + AM.getDireccion() + "\n"
+                + AM.getTelefono() + "\n"
+                + AM.getEmpleados() + "\n"
+                + AM.getAviones() + "\n");
+
     }
     
 }
--- Proyecto Final/src/proyecto/pkgfinal/Personal.java
+++ Proyecto Final/src/proyecto/pkgfinal/Personal.java
@@ -1,14 +1,98 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package proyecto.pkgfinal;
 
-/**
- *
- * @author Thomas-CM
- */
+
 public class Personal {
+    private int pilotos;
+    private int oficiales;
+    private int comisarios;
+    private int azafatas;
+    private int pasajeros;
+
+    public Personal(int pilotos, int oficiales, int comisarios, int azafatas, int pasajeros){
+        this.pilotos = pilotos ;
+        this.oficiales= oficiales;
+        this.comisarios = comisarios;
+        this.azafatas = azafatas;
+        this.pasajeros = pasajeros;
+  
+}
+
+    Personal() {
+        
+    }
+    
+
+
+    /**
+     * @return the pilotos
+     */
+    public int getPilotos() {
+        return pilotos;
+    }
+
+    /**
+     * @param pilotos the pilotos to set
+     */
+    public void setPilotos(int pilotos) {
+        this.pilotos = pilotos;
+    }
+
+    /**
+     * @return the oficiales
+     */
+    public int getOficiales() {
+        return oficiales;
+    }
+
+    /**
+     * @param oficiales the oficiales to set
+     */
+    public void setOficiales(int oficiales) {
+        this.oficiales = oficiales;
+    }
+
+    /**
+     * @return the comisarios
+     */
+    public int getComisarios() {
+        return comisarios;
+    }
+
+    /**
+     * @param comisarios the comisarios to set
+     */
+    public void setComisarios(int comisarios) {
+        this.comisarios = comisarios;
+    }
+
+    /**
+     * @return the azafatas
+     */
+    public int getAzafatas() {
+        return azafatas;
+    }
+
+    /**
+     * @param azafatas the azafatas to set
+     */
+    public void setAzafatas(int azafatas) {
+        this.azafatas = azafatas;
+    }
+
+    /**
+     * @return the pasajeros
+     */
+    public int getPasajeros() {
+        return pasajeros;
+    }
+
+    /**
+     * @param pasajeros the pasajeros to set
+     */
+    public void setPasajeros(int pasajeros) {
+        this.pasajeros = pasajeros;
+    }
+    
+    
     
 }
